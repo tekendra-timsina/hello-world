357dfe3b38cfbc9e8f71a7608736d7b09e2a47b4
Tekendra <bisu@mypc.home>
2022-08-07 22:07:48 +0300

    try again please

--- add.py
+++ add.py
@@ -1,2 +1,2 @@
-def add(a,b):
+def add_two_num(a,b):
     return a+b
--- test_add.py
+++ test_add.py
@@ -1,5 +1,5 @@
-from . import add_two_num
+from add import add_two_num
 
 def test_add_two_num():
     assert add_two_num(7,9)==16
-    assert add_two_num(3,3)==7
+    assert add_two_num(3,3)!=7
